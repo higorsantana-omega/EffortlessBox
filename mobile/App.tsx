b439fec7ba30f725b9d607d27fdc675bbddc103e
higorsantana-omega <higor.santanahs17@gmail.com>
2023-12-24 15:27:53 -0300

    chore: configure path mappings

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@components/Button';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Button />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function Button() {
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+}
